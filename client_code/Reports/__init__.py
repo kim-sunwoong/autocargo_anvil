7af8204a62d7aff3c35ce6fa5be4546efcaba88f
sunwoong.kim831@gmail.com <sunwoong.kim831@gmail.com>
2025-01-17 03:21:09 +0000

    Edited Sales and Reports

--- client_code/Reports/__init__.py
+++ client_code/Reports/__init__.py
@@ -48,6 +48,14 @@ class Reports(ReportsTemplate):
   def warehouse_map_click(self, points, **event_args):
     """This method is called when a data point is clicked."""
     pass
+
+  def drop_down_1_change(self, **event_args):
+    self.lb_robotid.text = self.drop_down_1.selected_value
+    if self.drop_down_1.selected_value == "A1-2":
+      self.lb_power.text = '75 %'
+      self.lb_status.text = "Deactivate"
+      self.lb_status.font_color = "#007EBC"
+    pass
     
 
     
